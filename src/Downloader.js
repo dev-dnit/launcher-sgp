a4fe1ac445f3014a07a0be91d893db64bc87b072
Guilherme Candido <guilhermecandidochagas@gmail.com>
2025-10-28 17:14:51 -0300

    fix(process): logica do headleDownload adcionada

--- src/Downloader.js
+++ src/Downloader.js
@@ -1,49 +1,87 @@
 const axios = require("axios");
+const fs = require("fs/promises");
 const fsSync = require("fs");
 const path = require("path");
-
-const {createDirectory} = require('./FileHelper');
+const { createDirectory, fileExists } = require("./FileHelper");
 const logger = require("./Logger");
 
-const url = process.env.EXECUTABLE_URL_DOWNLOAD;
-const destFolder = process.env.EXECUTABLE_LOCAL_FOLDER || "teste";
-const destFile = path.join(destFolder, process.env.EXECUTABLE_LOCAL_NAME || "teste");
+const {
+  REMOTE_CONFIG_URL,
+  GITHUB_TOKEN,
+  EXECUTABLE_URL_DOWNLOAD,
+  EXECUTABLE_LOCAL_FOLDER,
+  EXECUTABLE_LOCAL_NAME,
+} = process.env;
 
+const destFile = path.join(EXECUTABLE_LOCAL_FOLDER, EXECUTABLE_LOCAL_NAME);
 
 async function handleDownloadExecutableWithVersioning() {
-    // Colocar a lógica que verifica se a versão local é igual à versão remota
-    await downloadExecutable(); // #TODO: Melhorar essa lógica
-}
+  logger.info("Verificando a necessidade de atualização...");
 
+  const urlWithAuth = `${REMOTE_CONFIG_URL}?token=${GITHUB_TOKEN}`;
+  const urlWithCacheBuster = `${urlWithAuth}&v=${Date.now()}`;
 
-async function downloadExecutable() {
-    const dir = path.dirname(destFolder);
-    createDirectory(dir)
+  const response = await axios.get(urlWithCacheBuster);
+  const remoteConfig = response.data;
+  logger.info(`Versão remota encontrada: ${remoteConfig.version}`);
 
-    logger.info(`Baixando arquivo de ${url}...`);
+  const localConfigPath = path.join(
+    EXECUTABLE_LOCAL_FOLDER,
+    "local_config.json"
+  );
+  let localVersion = "0.0.0";
 
-    const writer = fsSync.createWriteStream(destFile);
-    const response = await axios({
-        method: "get",
-        url: `${url}?v=${Date.now()}`, // cache buster
-        responseType: "stream" }
+  if (await fileExists(localConfigPath)) {
+    try {
+      const localConfigFile = await fs.readFile(localConfigPath, "utf-8");
+      localVersion = JSON.parse(localConfigFile).version;
+    } catch (e) {
+      logger.warn(`Aviso: Falha ao ler JSON local. Erro: ${e.message}`);
+    }
+  }
+  logger.info(`Versão local encontrada: ${localVersion}`);
+
+  if (remoteConfig.version !== localVersion || !(await fileExists(destFile))) {
+    logger.info("Atualização necessária. Iniciando download...");
+    await downloadExecutable(remoteConfig.executable.url);
+    await fs.writeFile(localConfigPath, JSON.stringify(remoteConfig, null, 2));
+    logger.info(
+      `Configuração local atualizada para a versão ${remoteConfig.version}`
     );
+  } else {
+    logger.info("Nenhuma atualização de executável necessária.");
+  }
 
-    response.data.pipe(writer);
-    return new Promise((resolve, reject) => {
-        writer.on("finish", () => {
-            logger.info(`Arquivo salvo em: ${destFile}`);
-            resolve();
-        });
-        writer.on("error", (err) => {
-            logger.info("Falha ao salvar o arquivo.");
-            reject(err);
-        });
-    });
+  return remoteConfig.environment_variables;
 }
 
+async function downloadExecutable(downloadUrl) {
+  const url = downloadUrl || EXECUTABLE_URL_DOWNLOAD;
+  createDirectory(EXECUTABLE_LOCAL_FOLDER);
+
+  logger.info(`Baixando arquivo de ${url}...`);
+
+  const writer = fsSync.createWriteStream(destFile);
+  const response = await axios({
+    method: "get",
+    url: `${url}?v=${Date.now()}`, // cache buster
+    responseType: "stream",
+  });
+
+  response.data.pipe(writer);
+  return new Promise((resolve, reject) => {
+    writer.on("finish", () => {
+      logger.info(`Arquivo salvo em: ${destFile}`);
+      resolve();
+    });
+    writer.on("error", (err) => {
+      logger.error("Falha ao salvar o arquivo.", err);
+      reject(err);
+    });
+  });
+}
 
 module.exports = {
-    downloadExecutable,
-    handleDownloadExecutableWithVersioning
-}
+  downloadExecutable,
+  handleDownloadExecutableWithVersioning,
+};
