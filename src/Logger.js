bd180a6eb0a1a23252c503d2d36380582f8885b4
Guilherme Candido <guilhermecandidochagas@gmail.com>
2025-10-29 17:08:11 -0300

    fix(logger): Remove criação de diretório para compatibilidade com pkg" -m "A biblioteca pkg cria um sistema de arquivos virtual somente leitura, o que causava um erro 'Cannot mkdir in a snapshot'. A solução foi remover a lógica de criação de pasta (fs.mkdirSync), assumindo que o diretório de logs será criado manualmente no servidor.

--- src/Logger.js
+++ src/Logger.js
@@ -1,58 +1,35 @@
 const winston = require("winston");
 require("winston-daily-rotate-file");
 const path = require("path");
-const {createDirectory} = require('./FileHelper');
 
+const logDir = process.env.LOG_FOLDER;
 
-const logDir = path.join(__dirname, "../logs");
+if (!logDir) {
+  throw new Error(
+    "A variável de ambiente 'LOG_FOLDER' não está definida no arquivo .env."
+  );
+}
 
-
-// Define log format
-const logFormat = winston.format.printf(({timestamp, level, message}) => {
-    return `[${timestamp}] [${level}] ${message}`;
+const dailyRotateFileTransport = new winston.transports.DailyRotateFile({
+  filename: path.join(logDir, "sgp-launcher-%DATE%.log"),
+  datePattern: "YYYY-MM-DD",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
 });
 
-
-// File transport (daily rotation)
-const fileTransport = new winston.transports.DailyRotateFile({
-    dirname: logDir,
-    filename: "app-%DATE%.log",
-    datePattern: "YYYY-MM-DD",
-    zippedArchive: false,
-    maxFiles: "30d",
-    level: "info",
-    auditFile: null,
-    format: winston.format.combine(
-        winston.format.timestamp({format: "YYYY-MM-DD HH:mm:ss"}),
-        logFormat
-    ),
-});
-
-
-// Console transport
-const consoleTransport = new winston.transports.Console({
-    format: winston.format.combine(
-        winston.format.timestamp({format: "YYYY-MM-DD HH:mm:ss"}),
-        winston.format.colorize({all: false}), // color only the level
-        winston.format.printf(({timestamp, level, message}) => {
-            return `[${timestamp}] [${level}] ${message}`;
-        })
-    ),
-});
-
-
-// Create logger
 const logger = winston.createLogger({
-    level: "info",
-    format: winston.format.combine(
-        winston.format.timestamp({format: "YYYY-MM-DD HH:mm:ss"}),
-        logFormat
-    ),
-    transports: [fileTransport, consoleTransport],
-    exitOnError: false,
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.printf(
+      (info) =>
+        `[${info.timestamp}] [${info.level.toUpperCase()}] ${info.message}`
+    )
+  ),
+  transports: [new winston.transports.Console(), dailyRotateFileTransport],
 });
-
-
-createDirectory(logDir);
 
 module.exports = logger;
